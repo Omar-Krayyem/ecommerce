7ce61e8d06e7909776dbface50cf4c86812ce4a6
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-07-30 19:05:13 +0300

    Created all controllers <laravel>

--- ecommerce_server/ecommerce_laravel/routes/api.php
+++ ecommerce_server/ecommerce_laravel/routes/api.php
@@ -2,18 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\UsersController;
+use App\Http\Controllers\CustomersController;
+use App\Http\Controllers\AdminController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::get('/', function(){
+    return view('welcome');
+});
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/omar', function(){
+    echo "omar";
 });
